512f24c8031472b89cb10cae0dd69a3ca6244425
Andre Morryson <amorrysson77@gmail.com>
2024-03-04 14:22:22 -0300

    zod integration for typescript

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,16 @@
 import fastify from 'fastify'
+import { z } from 'zod'
 
 const app = fastify()
 
 app.post('/polls', (req) => {
+    const createPollBody = z.object({
+        title: z.string()
+    })
+
+    const { title } = createPollBody.parse(req.body)
+
     console.log(req.body)
-   
 })
 
 app.listen({ port:3333 }).then(() => {
