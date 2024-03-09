ac20cd54f51ce85bd1c7cb6ead32f39b7bb33305
Andre Morryson <amorrysson77@gmail.com>
2024-03-09 13:33:21 -0300

    Handle poll retrieval and vote counts retrieval

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { z } from "zod"
 import { prisma } from "../../lib/prisma"
 import { FastifyInstance } from "fastify"
+import { redis } from '../../lib/redis'
 
 export async function getPoll(app: FastifyInstance) {
     app.get('/polls/:pollId', async (req, reply) => {
@@ -9,20 +10,40 @@ export async function getPoll(app: FastifyInstance) {
         })
     
         const { pollId } = getPollParams.parse(req.params)
-    
+        
+        
         const poll = await prisma.poll.findUnique({
             where: {
                 id: pollId,
             },
             include: {
                 options: {
-                     select: {
+                    select: {
                         id: true,
                         title: true
                      }
-                }
+                    }
             }
         })
+        
+        if(!poll){
+        return reply.status(400).send({ message: 'Poll not found.' })
+        }
+
+        const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+        
+        const votes = result.reduce((obj, line, index) => {
+            if(index % 2 === 0 ) {
+                const score = result[index + 1]
+
+                Object.assign(obj, { [line]: Number(score) } )
+            }
+            
+            return obj
+
+        }, {} as Record<string, number>)
+
+        console.log(votes)
     
         return reply.send({ poll })
     })
